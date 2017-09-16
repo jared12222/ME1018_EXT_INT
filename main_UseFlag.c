bc8e1a424903bc6a13daf9d56b37f9eb7ad93552
XRAY <jared12222@gmail.com>
2017-09-16 09:21:44 +0800

    Try to explain the procedure of External Interrupt

--- main_UseFlag.c
+++ main_UseFlag.c
@@ -6,27 +6,30 @@
 #define reg_ExtINT_Flag         203
 #define reg_ExtINT_DDR_A        204
 #define reg_ExtINT_DDR_B        205
+
+#define INTn_Enable              1
+#define INTn_Disable             0
 //----------definition about KB00
 #define KB00_ID 2
 
 int main(int argc, char const *argv[]) {
     //-------------variables
-    char check;                    // for getting interrupt flag
+    char check,data;                    // for getting interrupt flag
     //-------------initial settings
     ASA_M128_set();                // initial settings for M128
-    M128_EXT_set(202,0x03,0,3);    // set INT4 triggered by low level
-    M128_EXT_set(204,0x10,4,0);    // set INT4 as input to be able be triggered
+    M128_EXT_set( reg_ExtINT_Mode_B , 0x03 , 0 , 3 );    // set INT4 triggered by low level
+    M128_EXT_set( reg_ExtINT_DDR_B  , 0x10 , 4 , 0 );    // set INT4 as input to be able be triggered
 
     while(1)
     {
         //------------ wait for triggered
         while(check)
         {
-            M128_EXT_fgt(203,0x10,4,&check);    // if flag is set, leave the loop
+            M128_EXT_fgt( reg_ExtINT_Flag , 0x10 , 4 , &check );    // if flag is set, leave the loop
         }
 
         //------------ after triggered
-        ASA_KB00_get( KB00_ID , char LSByte, char Bytes, void *Data_p);
+        ASA_KB00_get( KB00_ID , 100, 1 , &data);
 
         M128_EXT_fpt(203,0x10,4,1);    // clear the flag if interrupt isn't enable
     }
