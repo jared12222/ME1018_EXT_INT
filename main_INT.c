f4d85863d40e22a7f77e63a1d9f1375a973023c9
XRAY <jared12222@gmail.com>
2017-09-16 11:04:14 +0800

    Interrupt by using interrupt vector finished

--- main_INT.c
+++ main_INT.c
@@ -12,27 +12,34 @@
 //----------definition about KB00
 #define KB00_ID 2
 
+//----------function be executed when interrupt
+char data;
+char flag;
+void func(void)
+{
+    //------------ after triggered
+    ASA_KB00_get( KB00_ID , 100, 1 , &data);
+    flag = 1;
+}
+
 int main(int argc, char const *argv[]) {
     //-------------variables
-    char check,data;                    // for getting interrupt flag
+
     //-------------initial settings
-    ASA_M128_set();                // initial settings for M128
+    ASA_M128_set();                     // initial settings for M128
     M128_EXT_set( reg_ExtINT_Mode_B , 0x03 , 0 , 3 );    // set INT4 triggered by low level
     M128_EXT_set( reg_ExtINT_DDR_B  , 0x10 , 4 , 0 );    // set INT4 as input to be able be triggered
-    M128_EXT_set( reg_ExtINT_Enable, 0x01 , 0 , 1 );    // Enable INT4 interrupt
+    M128_EXT_isr( 4 , func);                             // Register function executed when triggered
+    M128_EXT_set( reg_ExtINT_Enable , 0x01 , 0 , 1 );    // Enable INT4 interrupt
 
     while(1)
     {
-        //------------ wait for triggered
-        while(check)
+        if(flag)
         {
-            M128_EXT_fgt( reg_ExtINT_Flag , 0x10 , 4 , &check );    // if flag is set, leave the loop
+            printf("Data: %d\n", data );
+            flag = 0;
         }
 
-        //------------ after triggered
-        ASA_KB00_get( KB00_ID , 100, 1 , &data);
-
-        M128_EXT_fpt(203,0x10,4,1);    // clear the flag if interrupt isn't enable
     }
     return 0;
 }
